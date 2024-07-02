2e8ebd8e5344437d9c691f2244559ede915a04ee
glmzhn <galymzhan2007@gmail.com>
2024-07-03 03:49:49 +0500

    Second1

--- service/myapp/models.py
+++ service/myapp/models.py
@@ -12,10 +12,8 @@ class Query(models.Model):
 
 
 class ExternalServerResponse(models.Model):
-    number = models.CharField(max_length=50, null=False, default=None)
-    latitude = models.IntegerField(null=False, default=None)
-    longitude = models.IntegerField(null=False, default=None)
+    query = models.OneToOneField(Query, on_delete=models.CASCADE)
     result = models.BooleanField()
 
     def __str__(self):
-        return f"Response for Query {self.number}"
+        return f"Response for Query {self.query.id}"
